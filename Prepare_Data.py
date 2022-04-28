cc1e43f889f2b06e1be1ed84cf823a4425db93cb
Dan <75431018+danbr123@users.noreply.github.com>
2022-04-28 18:28:59 +0300

    Add function to convert csv to json files

--- Prepare_Data.py
+++ Prepare_Data.py
@@ -5,7 +5,9 @@ from collections import OrderedDict
 import numpy as np
 
 
-base_dir = './resources'
+BASE_DIR = './resources'
+STATION_PARAMS_TO_USE = ["julian_day", "habitat", "wind_speed", "temperature", "humidity", "pressure", "radiation",
+                         "IR_temp"]
 
 data_map = OrderedDict({
     'Height': None,
@@ -16,7 +18,7 @@ data_map = OrderedDict({
     'SLP': None,
     'TGI': None,
 })
-listdir = [dir for dir in os.listdir(base_dir) if 'properties' not in dir]
+listdir = [dir for dir in os.listdir(BASE_DIR) if 'properties' not in dir]
 
 '''
     inputs to consider:
@@ -24,21 +26,21 @@ listdir = [dir for dir in os.listdir(base_dir) if 'properties' not in dir]
         - From Metro-Station: Julian Day, Day Time, Habitat, Wind Speed, Air Temperature, Ground Temperature Humidity, Pressure, Radiation.   
 '''
 
-inputs = len(data_map.keys()) + 8
+inputs = len(data_map.keys()) + len(STATION_PARAMS_TO_USE)
 samples = 1000
 X = np.zeros(shape=(samples * len(listdir), inputs))
 
 for dir in listdir:
     dir_data = data_map.copy()
-    dir_data['Height'] = np.array(Image.open('{base_dir}/{dir}/height.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['IR'] = np.array(Image.open('{base_dir}/{dir}/IR.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['RealSolar'] = np.array(Image.open('{base_dir}/{dir}/real_solar.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['Shade'] = np.array(Image.open('{base_dir}/{dir}/shade.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['SkyView'] = np.array(Image.open('{base_dir}/{dir}/sky_view.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['SLP'] = np.array(Image.open('{base_dir}/{dir}/SLP.tif'.format(base_dir=base_dir, dir=dir)))
-    dir_data['TGI'] = np.array(Image.open('{base_dir}/{dir}/TGI.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['Height'] = np.array(Image.open('{base_dir}/{dir}/height.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['IR'] = np.array(Image.open('{base_dir}/{dir}/IR.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['RealSolar'] = np.array(Image.open('{base_dir}/{dir}/real_solar.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['Shade'] = np.array(Image.open('{base_dir}/{dir}/shade.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['SkyView'] = np.array(Image.open('{base_dir}/{dir}/sky_view.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['SLP'] = np.array(Image.open('{base_dir}/{dir}/SLP.tif'.format(base_dir=BASE_DIR, dir=dir)))
+    dir_data['TGI'] = np.array(Image.open('{base_dir}/{dir}/TGI.tif'.format(base_dir=BASE_DIR, dir=dir)))
 
-    metro_station = json.loads('{base_dir}/{dir}/metro_station.json'.format(base_dir=base_dir, dir=dir))
+    metro_station = json.loads('{base_dir}/{dir}/station_data.json'.format(base_dir=BASE_DIR, dir=dir))
 
     row_indices = np.random.choice(1000, samples)
     col_indices = np.random.choice(1000, samples)
@@ -49,11 +51,11 @@ for dir in listdir:
             data_samples.append(image[i][j])
         X[k] = np.array(data_samples)
 
-
-
-
-
 print(1)
 
 
+def generate_data(samples, dir=None):
+    listdir = [dir] if dir else [dir for dir in os.listdir(BASE_DIR) if 'data_table' not in dir]
+
+def dir_to_dataset(dir):
 
--- utils.py
+++ utils.py
@@ -1,13 +1,27 @@
 import csv
 import json
+import os
 
 
 def csv_to_json(path):
+    rows = []
+    # read csv file
     with open(path, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         fields = next(csv_reader)
         for row in csv_reader:
-            data_json = {}
+            rows.append(row)
+
+    # generate json file for each row in the appropriate directory
+    for row in rows:
+        data_json = {}
+        for i in range(1, len(row)):
+            data_json[fields[i]] = row[i]
+        for option in ["", "_W", "_E"]:  # TODO: maybe make it more elegant
+            if row[0] + option in os.listdir("./resources"):
+                with open("./resources/{dir}/station_data.json".format(dir=row[0] + option), 'w') as data_file:
+                    json.dump(data_json, data_file, indent=4, separators=(',', ': '))
 
 
-csv_to_json("./resources/")
+
+csv_to_json("./resources/data_table.csv")
