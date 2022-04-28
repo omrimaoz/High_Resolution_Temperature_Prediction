bfa1499640ed36523ce8a34c23e6a196342c72d0
omrimaoz <omrimaoz@gmail.com>
2022-04-28 17:22:18 +0300

    Third commit - Work on Prepare_Data

--- Prepare_Data.py
+++ Prepare_Data.py
@@ -1,20 +1,56 @@
-import PIL
+import json
+import os
 from PIL import Image
-from matplotlib import pyplot as plt
+from collections import OrderedDict
 import numpy as np
-import tifffile as tiff
-import scipy
+
+
+base_dir = './resources'
+
+data_map = OrderedDict({
+    'Height': None,
+    'IR': None,
+    'RealSolar': None,
+    'Shade': None,
+    'SkyView': None,
+    'SLP': None,
+    'TGI': None,
+})
+listdir = [dir for dir in os.listdir(base_dir) if 'properties' not in dir]
+
+'''
+    inputs to consider:
+        - From data: Height (DSM), ..
+        - From Metro-Station: Julian Day, Day Time, Habitat, Wind Speed, Air Temperature, Ground Temperature Humidity, Pressure, Radiation.   
+'''
+
+inputs = len(data_map.keys()) + 8
+samples = 1000
+X = np.zeros(shape=(samples * len(listdir), inputs))
+
+for dir in listdir:
+    dir_data = data_map.copy()
+    dir_data['Height'] = np.array(Image.open('{base_dir}/{dir}/height.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['IR'] = np.array(Image.open('{base_dir}/{dir}/IR.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['RealSolar'] = np.array(Image.open('{base_dir}/{dir}/real_solar.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['Shade'] = np.array(Image.open('{base_dir}/{dir}/shade.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['SkyView'] = np.array(Image.open('{base_dir}/{dir}/sky_view.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['SLP'] = np.array(Image.open('{base_dir}/{dir}/SLP.tif'.format(base_dir=base_dir, dir=dir)))
+    dir_data['TGI'] = np.array(Image.open('{base_dir}/{dir}/TGI.tif'.format(base_dir=base_dir, dir=dir)))
+
+    metro_station = json.loads('{base_dir}/{dir}/metro_station.json'.format(base_dir=base_dir, dir=dir))
+
+    row_indices = np.random.choice(1000, samples)
+    col_indices = np.random.choice(1000, samples)
+
+    for k, i, j in enumerate(zip(row_indices, col_indices)):
+        data_samples = list()
+        for image in dir_data.values():
+            data_samples.append(image[i][j])
+        X[k] = np.array(data_samples)
 
 
 
-a = tiff.imread("./resources/Zeelim_23.9.19_1100_E/height.tif")
-img = scipy.misc.toimage(a)
-np.array(img)
-
-height_image = Image.open('./resources/Zeelim_23.9.19_1100_E/height.tif')
-height_matrix = np.array(height_image)
-normal_image = np.array(height_matrix / np.max(height_matrix) * 255, dtype=np.int)
-image = Image.fromarray(normal_image)
 
 
 print(1)
