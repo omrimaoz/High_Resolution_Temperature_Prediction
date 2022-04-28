0fe50da0bd8c5a221fa24b2a611362453117e4f7
omrimaoz <omrimaoz@gmail.com>
2022-04-28 15:47:18 +0300

    Second commit - load tiff incorrectly

--- Prepare_Data.py
+++ Prepare_Data.py
@@ -1,3 +1,23 @@
 import PIL
+from PIL import Image
+from matplotlib import pyplot as plt
+import numpy as np
+import tifffile as tiff
+import scipy
+
+
+
+a = tiff.imread("./resources/Zeelim_23.9.19_1100_E/height.tif")
+img = scipy.misc.toimage(a)
+np.array(img)
+
+height_image = Image.open('./resources/Zeelim_23.9.19_1100_E/height.tif')
+height_matrix = np.array(height_image)
+normal_image = np.array(height_matrix / np.max(height_matrix) * 255, dtype=np.int)
+image = Image.fromarray(normal_image)
+
+
+print(1)
+
 
 
