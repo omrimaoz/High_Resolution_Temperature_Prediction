89b23690f4dc918c98b3314130a1961cee8825d7
Dan <75431018+danbr123@users.noreply.github.com>
2022-05-04 16:57:18 +0300

    commit 7 - moved functionality to IRMaker object

--- IRMaker.py
+++ IRMaker.py
@@ -9,23 +9,17 @@ from torch.utils.data import DataLoader
 from Dataset import Dataset
 
 BASE_DIR = './resources'
-STATION_PARAMS_TO_USE = ["julian_day", 'time', "habitat", "wind_speed", "temperature", "humidity", "pressure", "radiation",
-                         "IR_temp"]
 
-data_map = OrderedDict({
-    'Height': None,
-    'RealSolar': None,
-    'Shade': None,
-    'SkyView': None,
-    'SLP': None,
-    'TGI': None,
-})
 
 class IRMaker(object):
+    STATION_PARAMS_TO_USE = ["julian_day", 'time', "habitat", "wind_speed", "temperature", "humidity", "pressure",
+                             "radiation",
+                             "IR_temp"]
+    data_maps = ['Height', 'RealSolar', 'Shade', 'SkyView', 'SLP', 'TGI']
+
     def __init__(self, dir, train):
         super(IRMaker, self).__init__()
         self.dir = dir
-
         self.Height = tiff.imread('{base_dir}/{dir}/height.tif'.format(base_dir=BASE_DIR, dir=dir))
         self.RealSolar = tiff.imread('{base_dir}/{dir}/real_solar.tif'.format(base_dir=BASE_DIR, dir=dir))
         self.Shade = tiff.imread('{base_dir}/{dir}/shade.tif'.format(base_dir=BASE_DIR, dir=dir))
@@ -48,7 +42,7 @@ class IRMaker(object):
         model.eval()
         predicted_IR = np.zeros_like(self.Height)
 
-        inputs = len(data_map.keys()) + len(STATION_PARAMS_TO_USE)
+        inputs = len(self.data_maps) + len(self.STATION_PARAMS_TO_USE)
         image_pixels = self.Height.shape[0] * self.Height.shape[1]
         X = np.zeros(shape=(image_pixels, inputs), dtype=np.float)
         batch_size = image_pixels // 10
@@ -60,19 +54,29 @@ class IRMaker(object):
         X[:, 4] = self.SLP.reshape(1, -1)
         X[:, 5] = self.TGI.reshape(1, -1)
 
-        for i, key in enumerate(STATION_PARAMS_TO_USE):
+        for i, key in enumerate(self.STATION_PARAMS_TO_USE):
             X[:, 6 + i] = np.tile(self.station_data[key], image_pixels)
 
         dataset = Dataset(X, np.zeros(shape=X.shape[0]))
         data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
+        i = 0
+        j = 0
         for x, y in data_loader:
             x, y = x.float(), y.float()
             y_hat = model(x).detach()
-            pred = np.array(y_hat.detach())
+            pred = np.array(y_hat.detach()).reshape(-1)
+            for pixel in pred:
+                predicted_IR[i][j] = pixel
+                j += 1
+                if j > predicted_IR.shape[1]:
+                    j = 0
+                    i += 1
+        tiff.imsave(self.dir + "/PredictedIR.tif", predicted_IR)
 
 
 
+    def get_data_dict(self):
+        return [self.Height, self.RealSolar, self.Shade, self.SkyView, self.SLP, self.TGI]
 
 
 
--- Prepare_Data.py
+++ Prepare_Data.py
@@ -5,11 +5,10 @@ import tifffile as tiff
 from PIL import Image
 from collections import OrderedDict
 import numpy as np
+from IRMaker import IRMaker
 
 
 BASE_DIR = './resources'
-STATION_PARAMS_TO_USE = ["julian_day", 'time', "habitat", "wind_speed", "temperature", "humidity", "pressure", "radiation",
-                         "IR_temp"]
 
 data_map = OrderedDict({
     'Height': None,
@@ -33,22 +32,25 @@ def prepare_data(samples, dir=None):
             - From Station: Julian Day, Day Time, Habitat, Wind Speed, Air Temperature, Ground Temperature Humidity, Pressure, Radiation.   
     '''
 
-    inputs = len(data_map.keys()) + len(STATION_PARAMS_TO_USE)
+    inputs = len(data_map.keys()) + len(IRMaker.STATION_PARAMS_TO_USE)
     X = np.zeros(shape=(samples * len(listdir), inputs), dtype=np.float)
     y = np.zeros(shape=(samples * len(listdir)), dtype=np.float)
     k = 0
 
     for dir in listdir:
-        dir_data, station_data, label_data = create_data(dir)
+        IRObj = IRMaker(dir, train=True)
+        dir_data = IRObj.get_data_dict()
+        station_data = IRObj.station_data
+        label_data = IRObj.IR
 
-        row_indices = np.random.choice(dir_data['Height'].shape[0], samples)
-        col_indices = np.random.choice(dir_data['Height'].shape[0], samples)
+        row_indices = np.random.choice(dir_data[0].shape[0], samples)
+        col_indices = np.random.choice(dir_data[0].shape[0], samples)
 
         for i, j in zip(row_indices, col_indices):
             data_samples = list()
-            for image in dir_data.values():
+            for image in dir_data:
                 data_samples.append(image[i][j])
-            for key in STATION_PARAMS_TO_USE:
+            for key in IRObj.STATION_PARAMS_TO_USE:
                 data_samples.append(station_data[key])
             X[k] = np.array(data_samples)
             y[k] = label_data[i][j]
@@ -57,21 +59,21 @@ def prepare_data(samples, dir=None):
     return X, y
 
 
-def create_data(dir):
-    dir_data = data_map.copy()
-    dir_data['Height'] = tiff.imread('{base_dir}/{dir}/height.tif'.format(base_dir=BASE_DIR, dir=dir))
-    dir_data['RealSolar'] = tiff.imread('{base_dir}/{dir}/real_solar.tif'.format(base_dir=BASE_DIR, dir=dir))
-    dir_data['Shade'] = tiff.imread('{base_dir}/{dir}/shade.tif'.format(base_dir=BASE_DIR, dir=dir))
-    dir_data['SkyView'] = tiff.imread('{base_dir}/{dir}/skyview.tiff'.format(base_dir=BASE_DIR, dir=dir))
-    dir_data['SLP'] = tiff.imread('{base_dir}/{dir}/SLP.tif'.format(base_dir=BASE_DIR, dir=dir))
-    dir_data['TGI'] = tiff.imread('{base_dir}/{dir}/TGI.tif'.format(base_dir=BASE_DIR, dir=dir))
-
-    label_data = tiff.imread('{base_dir}/{dir}/IR.tif'.format(base_dir=BASE_DIR, dir=dir)) + 273.15
-
-    with open('{base_dir}/{dir}/station_data.json'.format(base_dir=BASE_DIR, dir=dir), 'r') as f:
-        station_data = json.loads(f.read())
-
-    dir_data['RealSolar'] = np.average(dir_data['RealSolar'][1:-1, 1:-1]) * (dir_data['RealSolar'] < 0) * 1. +\
-                        dir_data['RealSolar'] * (dir_data['RealSolar'] >= 0) * 1.
-    return dir_data, station_data, label_data
+# def create_data(dir):
+#     dir_data = data_map.copy()
+#     dir_data['Height'] = tiff.imread('{base_dir}/{dir}/height.tif'.format(base_dir=BASE_DIR, dir=dir))
+#     dir_data['RealSolar'] = tiff.imread('{base_dir}/{dir}/real_solar.tif'.format(base_dir=BASE_DIR, dir=dir))
+#     dir_data['Shade'] = tiff.imread('{base_dir}/{dir}/shade.tif'.format(base_dir=BASE_DIR, dir=dir))
+#     dir_data['SkyView'] = tiff.imread('{base_dir}/{dir}/skyview.tiff'.format(base_dir=BASE_DIR, dir=dir))
+#     dir_data['SLP'] = tiff.imread('{base_dir}/{dir}/SLP.tif'.format(base_dir=BASE_DIR, dir=dir))
+#     dir_data['TGI'] = tiff.imread('{base_dir}/{dir}/TGI.tif'.format(base_dir=BASE_DIR, dir=dir))
+#
+#     label_data = tiff.imread('{base_dir}/{dir}/IR.tif'.format(base_dir=BASE_DIR, dir=dir)) + 273.15
+#
+#     with open('{base_dir}/{dir}/station_data.json'.format(base_dir=BASE_DIR, dir=dir), 'r') as f:
+#         station_data = json.loads(f.read())
+#
+#     dir_data['RealSolar'] = np.average(dir_data['RealSolar'][1:-1, 1:-1]) * (dir_data['RealSolar'] < 0) * 1. +\
+#                         dir_data['RealSolar'] * (dir_data['RealSolar'] >= 0) * 1.
+#     return dir_data, station_data, label_data
 
